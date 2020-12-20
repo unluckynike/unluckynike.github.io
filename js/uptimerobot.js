6d9b507965e7207e88efa1328b2ad36cf325cc20
WuLiZeng <53033075+unluckynike@users.noreply.github.com>
2020-12-20 21:36:12 +0800

    Site updated: 2020-12-20 21:36:11

--- js/uptimerobot.js
+++ js/uptimerobot.js
@@ -1,6 +1,7 @@
 var __apiKeys = [
 	'm786714954-b5d00325d9ee5ef11815e7c1',// API 1 吴李曾
-	'xxxxxxxxxx'// API 2
+    'm786715086-c282a0f8b91301ab48f7eff2',// API 2 栖枝
+    'm786715100-18172be827266ca882a787bd' //Mr.Chen
 ];
 //https://www.wulinzeng.vip/ 设置要监控的域名或者ip 获取到key
 
