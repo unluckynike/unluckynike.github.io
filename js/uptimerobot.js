95c2682e094f92191b697d91cd9f269412ba20cf
WuLiZeng <53033075+unluckynike@users.noreply.github.com>
2020-12-20 21:15:11 +0800

    Site updated: 2020-12-20 21:15:10

---
+++ js/uptimerobot.js
@@ -0,0 +1,9 @@
+var __apiKeys = [
+	'm786714954-b5d00325d9ee5ef11815e7c1',// API 1 吴李曾
+	'xxxxxxxxxx'// API 2
+];
+//https://www.wulinzeng.vip/ 设置要监控的域名或者ip 获取到key
+
+
+// refresh interval (in seconds)
+var __refresh = 300;
