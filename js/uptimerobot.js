6427c2cb5d34bc0f07b6fde4f9b6af047bf07824
WuLiZeng <53033075+unluckynike@users.noreply.github.com>
2020-12-20 22:21:31 +0800

    Site updated: 2020-12-20 22:21:30

--- js/uptimerobot.js
+++ js/uptimerobot.js
@@ -1,9 +1,24 @@
 var __apiKeys = [
 	'm786714954-b5d00325d9ee5ef11815e7c1',// API 1 吴李曾
     'm786715086-c282a0f8b91301ab48f7eff2',// API 2 栖枝
-    'm786715100-18172be827266ca882a787bd' //Mr.Chen
+    'm786715188-f647825a1a4147383f4f92a8',// Mr.Chen
+    'm786715129-97ef26f26fce0748897616f2',// 云玩家
+    'm786715130-15f583c90e9ced5e322e6899',// 零奇Knight Zero部落阁
+    'm786715133-bc58e4905b2231275bdc57a2',// Small-Rose
+    'm786715132-c94efe260c259bc31d33e5c0',// 大仙的梦居
+    'm786715144-2c342dbc6ee6274090a9f5ec',// ZEHAN
+    'm786715147-f4f0687919a2aee8d8180007',// 积木
+    'm786715143-6b1c4f1f71cecff86c79804f',// modao
+    'm786715152-20a9c5ffdd7d36fa95f3b73b',// TheBee
+    'm786715153-e58255c531a12cd7be9bd36a',// MiChong
+    'm786715150-d1af90fc991f06382289c3a4',// FoxScallion
+    'm786715156-8c56a8aac17c8efbc8e680c6',// DIYsay
+    'm786715158-b947e29e6a8158bd7fa9b263',// itsNekoDeng
+    'm786715155-a8b6fddba08a81e577ab6c20',// 时光海小屋
+    'm786715162-5f7c31a0d320996da2174363',// Mx
+    'm786715161-fe2246ee42083cc37e051ec1' //凡尘纪
 ];
-//https://www.wulinzeng.vip/ 设置要监控的域名或者ip 获取到key
+//https://uptimerobot.com/ 设置要监控的域名或者ip 获取到key
 
 
 // refresh interval (in seconds)
